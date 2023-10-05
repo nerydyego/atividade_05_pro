1a9839e426ad4f59cebae8f328b5d2baa581a58d
nerydyego <dyegonery@gmail.com>
2023-10-05 16:38:39 -0300

    Correção Linter - arquivo funcao.py

--- mentorama_modulo_5_pro/funcao.py
+++ mentorama_modulo_5_pro/funcao.py
@@ -1,11 +1,21 @@
-
+"""
+Função de numeros multiplos de 5 e 7
+"""
 class NumerosPrimos:
-    def multiplos(self, var1):
-        if(var1 % 5 == 0) and (var1 % 7 == 0):
+    def multiplos(self, var1):    
+        """ Verifica se um número é múltiplo de 5 e/ou 7. 
+    Args: 
+        var1 (int): O número a ser verificado.
+    Returns:
+        str: 'FizzBuzz' se var1 for múltiplo de ambos 5 e 7.
+             'Fizz' se var1 for múltiplo de 5.
+             'Buzz' se var1 for múltiplo de 7.
+             'Miss' caso contrário.
+        """
+        if var1 % 5 == 0 and var1 % 7 == 0:
             return 'FizzBuzz'
-        elif (var1 % 5 == 0):
+        if var1 % 5 == 0:
             return "Fizz"
-        elif (var1 % 7 == 0):
+        if var1 % 7 == 0:
             return "Buzz"
-        else:
-            return "Miss"
+        return "Miss"
